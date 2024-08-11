fa3eb979b81228912933d843fa162cec5bc0a5d9
Shane Panter <shane@foundationcode.com>
2024-08-11 17:00:11 -0600

    Added minor change to test file

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ import {sayHello} from '../js/main.js';
 
 QUnit.module('hello', function() {
     QUnit.test('make sure the hello function says hello', function(assert) {
-        assert.equal(sayHello(), 'hello');
+        var result = sayHello();
+        assert.equal(result, 'hello');
     });
 });
